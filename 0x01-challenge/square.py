6788c7dc329912383f43dbbeaae1ce1e910ad00f
Rashnotech <rashnotech@gmail.com>
2023-10-17 23:39:54 +0000

    Fixed: code format

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -2,26 +2,28 @@
 """ a module that find the square """
 
 
-class Square:
+class Square():
     """
     A class that perform geometry
     Args:
         width: width of the square
         height: height of the square
     """
-    def __init__(self, *args, **kwargs):
-        """ initialization """
-        if kwargs:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
+    width = 0
+    height = 0
 
-    def area(self):
+    def __init__(self, *args, **kwargs):
+        """Initialization """
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def area_of_my_square(self):
         """ Area of the square """
         return self.width * self.width
 
-    def perimeter(self):
-        """ Perimeter of the square """
-        return 2 * (self.width + self.height)
+    def PermiterOfMySquare(self):
+        """ perimeter of the square """
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
         """ Print the str version of square"""
@@ -29,8 +31,7 @@ class Square:
 
 
 if __name__ == "__main__":
-
-    s = Square(width=12, height=9)
+    s = square(width=12, height=9)
     print(s)
-    print(s.area())
-    print(s.perimeter())
+    print(s.area_of_my_square())
+    print(s.PermiterOfMySquare())
